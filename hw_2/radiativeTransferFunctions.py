0fb2ce2d026692e60f54b92bdd7ca4b292d60038
Caleb Choban <calebchoban@gmail.com>
2018-10-09 10:22:04 -0700

    Added calcOpticalDepth and calcCrossSection and add print out for problem 1

--- hw_2/radiativeTransferFunctions.py
+++ hw_2/radiativeTransferFunctions.py
@@ -1,1 +1,10 @@
+import numpy as np 
 
+# Calculates column density of uniform density cloud of a given depth
+# Give parameters in cgs units
+def calcColumnDensity(density, distance):
+	return density*distance
+
+# Finds cross section for given optical depth, density, and distance
+def calcCrossSection(density, opt_depth, distance):
+	return opt_depth / calcColumnDensity(density, distance)
--- hw_2/run.py
+++ hw_2/run.py
@@ -1,1 +1,12 @@
+from radiativeTransferFunctions import *
 
+pcToCm = 3.086E18
+
+# all parameters given in cgs
+density = 1; depth = 100*pcToCm;
+
+print "Problem 1"
+print "Column density of cloud is", calcColumnDensity(density,depth), "cm^-2"
+print "a) cross section =", calcCrossSection(density, 10**-3, depth), "cm^2"
+print "b) cross section =", calcCrossSection(density, 1, depth), "cm^2"
+print "c) cross section =", calcCrossSection(density, 10**3, depth), "cm^2"

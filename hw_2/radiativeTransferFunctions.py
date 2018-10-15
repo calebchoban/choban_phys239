3db0c664a73b144246564b57881a208aa107ff2f
Caleb Choban <calebchoban@gmail.com>
2018-10-15 16:35:06 -0700

    Created calcSpecIntens which calculates the specific intensity through a cloud for one frequency

--- hw_2/radiativeTransferFunctions.py
+++ hw_2/radiativeTransferFunctions.py
@@ -8,3 +8,20 @@ def calcColumnDensity(density, distance):
 # Finds cross section for given optical depth, density, and distance
 def calcCrossSection(density, opt_depth, distance):
 	return opt_depth / calcColumnDensity(density, distance)
+
+# Calculates change in intensity of light rays of a given intial intensity, 
+# through a cloud of a given cross section, depth, and source.
+def calcSpecIntens(init_intens, cross_sec, source, distance, density, steps):
+	ds = distance/steps # step size
+	intens = np.zeros(steps)
+	positions = np.arange(0, distance+ds, ds)
+
+	for i, val in enumerate(intens):
+		if i == 0:
+			intens[i] = init_intens
+			continue
+		pos = positions[i]
+		opt_depth  = cross_sec * calcColumnDensity(density, ds)
+		intens[i] = intens[i-1]*np.exp(-opt_depth) + source*(1-np.exp(-opt_depth))
+
+	return intens
--- hw_2/run.py
+++ hw_2/run.py
@@ -10,3 +10,8 @@ print "Column density of cloud is", calcColumnDensity(density,depth), "cm^-2"
 print "a) cross section =", calcCrossSection(density, 10**-3, depth), "cm^2"
 print "b) cross section =", calcCrossSection(density, 1, depth), "cm^2"
 print "c) cross section =", calcCrossSection(density, 10**3, depth), "cm^2"
+
+source = 1; init_intens=0;
+cross_sec = calcCrossSection(density, 10**0, depth)
+print "Problem 2"
+print calcSpecIntens(init_intens, cross_sec, source, depth, density, 100)
